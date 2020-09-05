f98d4b52be6e1194f78970663e6b0a2af9a024b0
James Leong <jameszleong@gmail.com>
2020-09-05 11:32:11 +0800

    changed project image

--- src/Sections/Projects.js
+++ src/Sections/Projects.js
@@ -24,7 +24,7 @@ function Projects() {
                 <img
                     className="slide-image music-player"
                     alt="music player"
-                    src="https://res.cloudinary.com/df95voyec/image/upload/v1599217845/Personal%20Website/Screen_Shot_2020-09-04_at_6.41.19_PM_jyimca.png"
+                    src="https://res.cloudinary.com/df95voyec/image/upload/v1599276640/Personal%20Website/Screen_Shot_2020-09-05_at_11.29.34_AM_aukpk0.png"
                 />
                 <Carousel.Caption>
                     <a href={"https://github.com/JamesLeong98/scotch-music-player"} target={"_blank"} rel={"noopener noreferrer"}><h4>Scotch Music Player</h4></a>
