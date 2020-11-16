b1d17eafd3810949b0c3a580a214da9b33c3a03e
James Leong <jameszleong@gmail.com>
2020-11-16 17:53:50 +0800

    prod build

---
+++ build/precache-manifest.7ec14129e1df7f7a8dc32c17bf7f423d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb9b5140f1a8f01e6fb1034913412b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29bfc9662a8b566a29ca",
+    "url": "/static/css/main.71fcc62e.chunk.css"
+  },
+  {
+    "revision": "81c14a88c51549a1889b",
+    "url": "/static/js/2.1796b29f.chunk.js"
+  },
+  {
+    "revision": "1119c0de3de88786d5f4709116393eff",
+    "url": "/static/js/2.1796b29f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29bfc9662a8b566a29ca",
+    "url": "/static/js/main.d1cf2732.chunk.js"
+  },
+  {
+    "revision": "f57bd6b3cd488554ebce",
+    "url": "/static/js/runtime-main.39870823.js"
+  }
+]);
